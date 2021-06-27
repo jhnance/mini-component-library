52083f79c736c599e4c17a8f84b651411eed0be2
Joshua Nance <joshua.h.nance@gmail.com>
2021-06-27 16:30:18 -0500

    Implement a very strange solution that hides a real select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,53 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+function Select({ label, value, onChange, children }) {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <Wrapper label={label}>
+        <FlexWrapper>
+          {displayedValue}
+          <Icon id="chevron-down" className="leftPadding" />
+          <HiddenSelect onChange={onChange}>{children}</HiddenSelect>
+        </FlexWrapper>
+      </Wrapper>
+    </>
   );
-};
+}
+
+const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+
+  .leftPadding {
+    padding-left: 15px;
+  }
+`;
+
+const HiddenSelect = styled.select`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0;
+`;
+
+const Wrapper = styled.div`
+  display: inline-block;
+  width: auto;
+  height: 43px;
+  padding: 12px 16px;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  color: ${COLORS.gray700};
+  position: relative;
+`;
 
 export default Select;
